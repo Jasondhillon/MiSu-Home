c2a83f6d1953e18ca0a4826e3c1c6acc5018f558
BlakelyBarr <b.barr@knights.ucf.edu>
2020-12-03 03:49:33 -0500

    makes a change to read only properties so if there is a value it displays it instead of just 'read only'

--- components/cards/DeviceCard.js
+++ components/cards/DeviceCard.js
@@ -680,7 +680,7 @@ class DeviceCard extends Component {
                           
                           {/* Render Property Readonly */}
                           {prop.read_only == 1 && 
-                            <AppText style={{fontStyle: 'italic', fontSize:16}}> Read Only</AppText>
+                                        <AppText style={{ fontStyle: 'italic', fontSize: 16 }}> {prop.value == '' ? 'Read only': prop.value}</AppText>
                           }
         
                           {/* Render Switch for Boolean */}
